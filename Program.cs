a2c8168b808887d8cdb5a0ec39241e7b17066721
deweloper12 <154529833+deweloper12@users.noreply.github.com>
2025-06-24 10:43:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Chips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "chips: [1, 5, 9, 10, 5]";
+            if (input.StartsWith("chips:") && input.EndsWith("]"))
+            {
+                int bracket = input.IndexOf('[');
+                input = input.Substring(bracket + 1, input.Length - bracket - 2);
+            }
+
+            string[] chipsStrs = input.Split(',');
+            int[] chips = new int[chipsStrs.Length];
+            for (int i = 0; i < chipsStrs.Length; i++)
+            {
+                chips[i] = Convert.ToInt16(chipsStrs[i]);
+            }
+
+            Calc calc = new Calc(chips);
+            int steps = calc.getSteps();
+
+            Console.WriteLine(steps);
+
+            Console.Read();
+        }
+
+    }
+
+    class Calc
+    {
+        int[] chips;
+        int  middle;
+        int steps = 0;
+
+        public Calc(int[] _chips)
+        {
+            chips = _chips;
+        }
+
+        public int getSteps()
+        {
+            calcMiddle();
+
+
+            replace();
+
+            return steps;
+        }
+
+        private int maxIndex()
+        {
+            int maxIndex = 0;
+            int max = 0;
+            for (int i = 0; i < chips.Length; i++)
+            {
+                if (chips[i] > max)
+                {
+                    max = chips[i];
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        private void replace()
+        {
+            int max = maxIndex();
+
+            if (chips[max] == middle)
+            {
+                return;
+            }
+            else
+            {
+                int left = leftIndex(max);
+                int maxRight = maxRightIndex(max);
+                int right = rightIndex(maxRight);
+
+                if (chips[left] < chips[right])
+                {
+                    chips[left]++;
+                    chips[max]--;
+                }
+                else
+                {
+                    chips[right]++;
+                    chips[maxRight]--;
+                }
+                
+            }
+            steps++;
+
+            replace();
+        }
+
+        private int maxRightIndex(int max)         //Правая граница последовательности максимальных значений
+        {
+            int maxRight = max;
+            for (int i = max + 1; i < chips.Length ; i++)
+            {
+                if (chips[i] == chips[max])
+                {
+                    maxRight++;
+                }
+            }
+
+            return maxRight;
+        }
+
+        private int leftIndex(int maxIndex)
+        {
+            if (maxIndex > 0)
+            {
+                return maxIndex - 1;
+            }
+            else
+            {
+                return chips.Length-1;
+            }
+        }
+        private int rightIndex(int maxIndex)
+        {
+            if (maxIndex < chips.Length-1)
+            {
+                return maxIndex + 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void calcMiddle()
+        {
+            int sum = 0;
+            foreach (int chip in chips)
+            {
+                sum += chip;
+            }
+            middle = sum / chips.Length;
+        }
+
+        private bool isEqual()
+        {
+            foreach (int chip in chips)
+            {
+                if (chip != middle)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
